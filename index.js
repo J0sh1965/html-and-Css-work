0a346d794b4b815d3410d6c418a3cf738ee640a7
J0sh1965 <muwanguzi.jonathan.upti@gmail.com>
2025-03-24 14:59:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//A variable is a container that stores data values 
+/*let age = 23
+console.log(age)
+let
+const
+var
+const age = 200;
+age = 30
+console.log(age)
+Data Types
+String = "jonathan"
+Numbers = 23
+Boolean true/false
+Objects = pen{}
+null
+undefined
+Array
+
+let names = ["Josh","andre","Jaban","Nigga"]*/
+// let heading = document.getElementById("myh1").textContent = "Common food in loacal Areas";
+// console.log(heading)
+// let my1stParagraph = document.getElementById("1stp").textContent = "Cabage";
+// let my2ndParagraph = document.getElementById("2ndp").textContent = "Mangoes";
+let my3rdParagraph = document.getElementById("3rdp").textContent = "Carrots";
+
